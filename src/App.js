05927259db7738b97172c8cf4e7346a6a6f3507e
JorgeKunrath <54911848+JorgeKunrath@users.noreply.github.com>
2022-01-18 00:05:43 -0300

    add basic structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import "./styles.css";
+import styled from "styled-components";
+
+const Header = styled.div`
+  border: 2px solid blue;
+  text-align: center;
+`;
+
+const Grid = styled.section`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1rem;
+`;
+
+const Card = styled.a`
+  border: 2px solid black;
+  font-weight: 1.5rem;
+`;
+
+const proposals = [
+  { title: "title 1", emoji: "😀", model: "title-1" },
+  { title: "title 2", emoji: "😁", model: "title-2" },
+  { title: "title 3", emoji: "😚", model: "title-3" },
+  { title: "title 4", emoji: "🤐", model: "title-4" },
+  { title: "title 5", emoji: "😎", model: "title-5" }
+];
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Header>JB</Header>
+      <h1>Bem vindo ao gerador de propostas da Juice Box etc</h1>
+
+      <Grid>
+        {proposals.map((proposal) => (
+          <Card key={"/" + proposal.model} href={proposal.model}>
+            {proposal.emoji} - {proposal.title}
+          </Card>
+        ))}
+      </Grid>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
 import App from "./App";
+
+const Demo = () => (
+  <>
+    <Link to="/">voltar</Link>
+  </>
+);
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/title-1" element={<Demo />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
   rootElement
 );
